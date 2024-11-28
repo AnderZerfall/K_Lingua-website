040fd7f7b1b45ae9e9c40b4020a6a4b3a5b3cbf1
Fukuro <anya.androschuk@gmail.com>
2024-11-28 17:12:48 +0200

    fixes

--- scripts/sendForm.js
+++ scripts/sendForm.js
@@ -1,28 +1,28 @@
-window.addEventListener('load', () => {
-    const form = document.getElementById('contact-form');
+// window.addEventListener('load', () => {
+//     const form = document.getElementById('contact-form');
 
-    function sendData(form) {
-        console.log('Start the function');
-        const XHR = new XMLHttpRequest();
+//     function sendData(form) {
+//         console.log('Start the function');
+//         const XHR = new XMLHttpRequest();
 
-        const formData = new FormData(form);
+//         const formData = new FormData(form);
 
-        XHR.addEventListener('load', (event) => {
-            alert(event.target.responseText);
-        })
+//         XHR.addEventListener('load', (event) => {
+//             alert(event.target.responseText);
+//         })
 
-        XHR.addEventListener("error", (event) => {
-            alert("Oops! Something went wrong.");
-        });
+//         XHR.addEventListener("error", (event) => {
+//             alert("Oops! Something went wrong.");
+//         });
 
-        XHR.open("POST", "sendForm.php");
+//         XHR.open("POST", "sendForm.php");
 
-        XHR.send(formData);
-    }
+//         XHR.send(formData);
+//     }
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        console.log('event registered');
-        sendData();
-    });
-})
+//     form.addEventListener("submit", function (event) {
+//         event.preventDefault();
+//         console.log('event registered');
+//         sendData();
+//     });
+// })
